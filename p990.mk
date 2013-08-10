97bf26c59aba3398c6e11fa1741094523a757282
Randolf Kastenmaier <randolf.kastenmaier@gmail.com>
2013-08-10 23:12:23 +0200

    Update p990.mk

--- p990.mk
+++ p990.mk
@@ -35,12 +35,12 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/check_sdcard.sh:system/bin/check_sdcard.sh \
     $(LOCAL_PATH)/lgdrm.img:system/bin/lgdrm.img
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_KERNEL):kernel \
-    $(LOCAL_PATH)/kernel/bthid.ko:system/lib/modules/bthid.ko \
-    $(LOCAL_PATH)/kernel/scsi_wait_scan.ko:system/lib/modules/scsi_wait_scan.ko \
-    $(LOCAL_PATH)/kernel/wireless.ko:system/lib/modules/wireless.ko \
-    $(LOCAL_PATH)/kernel/zram.ko:system/lib/modules/zram.ko
+#PRODUCT_COPY_FILES += \
+#    $(LOCAL_KERNEL):kernel \
+#    $(LOCAL_PATH)/kernel/bthid.ko:system/lib/modules/bthid.ko \
+#    $(LOCAL_PATH)/kernel/scsi_wait_scan.ko:system/lib/modules/scsi_wait_scan.ko \
+#    $(LOCAL_PATH)/kernel/wireless.ko:system/lib/modules/wireless.ko \
+#    $(LOCAL_PATH)/kernel/zram.ko:system/lib/modules/zram.ko
 
 
 PRODUCT_PACKAGES += \
